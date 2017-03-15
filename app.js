54c0138cab70c4db7de625b0048a19d61557c426
Blake Schwartz <blakehschwartz@gmail.com>
2017-03-14 22:42:32 -0700

    change to app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
